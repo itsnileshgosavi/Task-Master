bc0b03914b2a07ee5b428465de7e66a6f194d966
Nilesh <nbgosavi5@gmail.com>
2024-01-14 16:11:39 +0530

    fixed jwt verify issue

--- src/app/api/users/tasks/route.js
+++ src/app/api/users/tasks/route.js
@@ -1,22 +1,31 @@
-import jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { Task } from "@/models/task";
 import { NextResponse } from "next/server";
 import { getResponseMessage } from "@/helper/getResponseMessage";
 
-export async function GET(request){
-    const token= request.cookies.get("loginToken")?.value;
-    const data= jwt.verify(token, process.env.JWT_KEY);
-    
+export async function GET(request) {
+  try {
+    const token = request.cookies.get("loginToken")?.value;
 
-    try {
-        const user_Id =data.userID;
-        const userstasks = await Task.find({
-            userID:user_Id
-        });
-        
-        return NextResponse.json(userstasks);
-    } catch (error) {
-        console.log(error);
-        return getResponseMessage("error getting user's tasks", false,500);
+    if (!token) {
+      return getResponseMessage("Token not provided", false, 401);
     }
+
+    const data = jwt.verify(token, process.env.JWT_KEY);
+
+    if (!data || !data.userID) {
+      return getResponseMessage("Invalid token data", false, 401);
+    }
+
+    const user_Id = data.userID;
+    const userstasks = await Task.find({
+      userID: user_Id
+    });
+
+    return NextResponse.json(userstasks);
+  } catch (error) {
+    console.error("Error verifying token:", error);
+
+    return getResponseMessage("Error getting user's tasks", false, 500);
+  }
 }
